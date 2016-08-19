020cd1c411edcdacb8e3ef986a4dbb08c24186aa
Jeffrey McDaniel <jmcda001@ucr.edu>
2016-08-19 11:41:51 -0700

    Create visitor.h
    Initialize the repo with the starter code

---
+++ visitor.h
@@ -0,0 +1,16 @@
+class PrintVisitor : public Visitor {
+	private:
+		std::string output;
+
+	public:
+		PrintVisitor();
+
+		void rootNode();		//For visiting a root node (do nothing)
+		void sqrNode();		//For visiting a square node
+		void multNode();		//For visiting a multiple node
+		void subNode();		//For visiting a subtraction node
+		void addNode();		//For visiting an add node
+		void opNode(Op* op);	//For visiting a leaf node
+
+		void execute();		#Prints all visited nodes
+};
