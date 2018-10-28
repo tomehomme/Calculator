2bad9bc6be5619180885f5808966f1f16e6c8521
brrcrites <brrcrites@gmail.com>
2018-10-27 21:22:43 -0700

    Minor visitor refactor

--- visitor.h
+++ visitor.h
@@ -5,12 +5,12 @@ class PrintVisitor : public Visitor {
 	public:
 		PrintVisitor();
 
-		void rootNode();	//For visiting a root node (do nothing)
-		void sqrNode();		//For visiting a square node
-		void multNode();	//For visiting a multiple node
-		void subNode();		//For visiting a subtraction node
-		void addNode();		//For visiting an add node
-		void opNode(Op* op);	//For visiting a leaf node
+		void rootNode(); //For visiting a root node (do nothing)
+		void sqrNode(); //For visiting a square node
+		void multNode(); //For visiting a multiple node
+		void subNode(); //For visiting a subtraction node
+		void addNode(); //For visiting an add node
+		void opNode(Op* op); //For visiting a leaf node
 
-		void execute();		//Prints all visited nodes
+		void execute(); //Prints all visited nodes
 };
