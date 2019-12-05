c7812486aae325582c6a7348aff33c62f3cfde2b
Paris Hom <phom001@ucr.edu>
2019-12-05 11:58:37 -0800

    Update visitor_test.hpp

--- visitor_test.hpp
+++ visitor_test.hpp
@@ -1,3 +1,4 @@
+
 #ifndef __VISITOR_TEST_HPP__
 #define __VISITOR_TEST_HPP__
 
@@ -5,20 +6,18 @@
 #include "visitor.hpp"
 #include "iterator.hpp"
 #include "base.hpp"
+
 #include "preorder_iterator.cpp"
 
 TEST(VisitorTest, NullIterator){
 	int count = 0;
 	Base* op1 = new Op(1);
 	NullIterator* test = new NullIterator(op1);
-//	test->first();
-	while(!test->is_done()){
-	//	cout << test->current()->stringify()<<endl;
-		test->next();
+	if(test->current()==nullptr){
 		count++;
 	}
 
-	EXPECT_EQ(nullptr,test->current());
+	EXPECT_EQ(1,count);
 
 }
 
@@ -26,21 +25,34 @@ TEST(VisitorTest, BinaryIterator){
 	Base* op1 = new Op(3);
 	Base* op2 = new Op(4);
 	Base* add = new Add(op1,op2);
-//	CountVisitor* counts = new CountVisitor();
+	CountVisitor* counts = new CountVisitor();
+	BinaryIterator* trav = new BinaryIterator(add);
+	trav->first();
+
+	while(!trav->is_done()){
+		trav->current()->accept(counts);
+		trav->next();
+	}
 	
+	EXPECT_EQ(2,counts->op_count());
 }
 
 TEST(VisitorTest, UnaryIterator){
-//	CountVisitor* counts = new CountVisitor();
+        Base* op1 = new Op(3);
+        Base* op2 = new Op(-4);
+        Base* add = new Add(op1,op2);
+	
+	UnaryIterator* trav = new UnaryIterator(new Abs(new Abs(add)));
+
+	CountVisitor* counts = new CountVisitor();
+	while(!trav->is_done()){
+		trav->current()->accept(counts);
+		trav->next();
+	}
+
+	EXPECT_EQ(1,counts->abs_count());
 		
 }
 
-TEST(VisitorTest, PreorderIterator){
-	Base* op1 = new Op(4.1);
-	Base* op2 = new Op(6);
-	Base* mult = new Mult(op1,op2);
-//	Base ceil = new Ceil(mult);	
-	
-}
 
 #endif
